7139de04ffc9fbf2743085a264abc625c83d79b9
Kyle Laplante <kyle.laplante@gmail.com>
2012-07-13 18:32:29 -0700

    first commit

---
+++ cm.mk
@@ -0,0 +1,18 @@
+$(call inherit-product, device/samsung/skyrocket/full_skyrocket.mk)
+
+# Inherit some common CM stuff.
+#$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Enhanced NFC
+#$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+# Inherit some common CM stuff.
+#$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-I727 TARGET_DEVICE=SGH-I727 BUILD_FINGERPRINT="samsung/SGH-I727/SGH-I727:4.0.4/IMM76D/UCLE2:user/release-keys" PRIVATE_BUILD_DESC="SGH-I727-user 4.0.4 IMM76D UCLE2 release-keys"
+
+TARGET_BOOTANIMATION_NAME := vertical-480x800
+
+PRODUCT_NAME := full_skyrocket
+PRODUCT_DEVICE := skyrocket
+
